28263919aa4623bf2cc996a14a2790b270ba2b18
Nat Quayle Nelson <natquaylenelson@gmail.com>
2025-08-08 10:24:13 -0500

    WIP parse out a control hierarchy

--- src/shortcutter/Main.hx
+++ src/shortcutter/Main.hx
@@ -1,5 +1,11 @@
 package shortcutter;
 
+typedef Hierarchy = {
+    key:String,
+    selector:String,
+    children:Array<Hierarchy>
+};
+
 class Main {
     static function main() {
         Main_.main();
